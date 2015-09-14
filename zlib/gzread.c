f35f22fan/QOds

commit 52d7769eaeb0d2220469932180755d4538a3174d

Added missing includes

zlib/gzread.c
@@ -5,6 +5,8 @@
 
 #include "gzguts.h"
 
+#include <unistd.h>
+
 /* Local functions */
 local int gz_load OF((gz_statep, unsigned char *, unsigned, unsigned *));
 local int gz_avail OF((gz_statep));

zlib/gzwrite.c
@@ -4,6 +4,7 @@
  */
 
 #include "gzguts.h"
+#include <unistd.h>
 
 /* Local functions */
 local int gz_init OF((gz_statep));

commit 84dfe0ca9f544497036cbd6c817bde1fd5ed65c1

Make compile on Windows
Broken by 52d7769eaeb0d2220469932180755d4538a3174d

zlib/gzread.c
@@ -4,8 +4,11 @@
  */
 
 #include "gzguts.h"
+#include "zlib.h"
 
-#include <unistd.h>
+#if defined(Z_HAVE_UNISTD_H)
+#  include <unistd.h>
+#endif
 
 /* Local functions */
 local int gz_load OF((gz_statep, unsigned char *, unsigned, unsigned *));

zlib/gzwrite.c
@@ -4,7 +4,11 @@
  */
 
 #include "gzguts.h"
-#include <unistd.h>
+#include "zlib.h"
+
+#if defined(Z_HAVE_UNISTD_H)
+#  include <unistd.h>
+#endif
 
 /* Local functions */
 local int gz_init OF((gz_statep));